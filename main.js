baf3b781f470c36abd4ea600bcd879b62fd58683
cabal82 <fabriziogallici@gmail.com>
2019-11-08 14:18:37 +0100

    init

---
+++ main.js
@@ -0,0 +1,8 @@
+/* RI - Creare la navbar con i vari dropdown del sito: https://www.revolut.com/en-IT
+I dropdown però appaiono e scompaiono al click e non al rollover.
+Cerco poi di gestire il fatto che possa essere aperto 1 solo dropdown alla volta. */
+
+
+$(document).ready(function () {
+  
+}
