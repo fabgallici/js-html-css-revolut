3d174a84057215e205c2bff895897a42bfd5129c
cabal82 <fabriziogallici@gmail.com>
2019-11-08 18:22:15 +0100

    dropdown work

--- main.js
+++ main.js
@@ -5,4 +5,16 @@ Cerco poi di gestire il fatto che possa essere aperto 1 solo dropdown alla volta
 
 $(document).ready(function () {
   
-}
+
+  $('.dropdown').click(function() {
+    // var clickedMenu = $(this).find('dropdown-content');
+    // console.log(clickedMenu);
+    // clickedMenu.toggle('menu-show');
+    // $('.dropdown').find('dropdown-content').addClass('menu-show');
+
+    // $(".dropdown-content").toggle();
+    // $('.dropdown .dropdown-content').toggleClass('menu-show');
+    $(this).find('.dropdown-content').toggleClass('menu-show');
+  });
+
+});
