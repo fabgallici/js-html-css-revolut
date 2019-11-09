f294a0f5cdbedd0311c94131a9ba8fe561fb032f
cabal82 <fabriziogallici@gmail.com>
2019-11-09 15:52:44 +0100

    3rd hamg dropdown

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ Cerco poi di gestire il fatto che possa essere aperto 1 solo dropdown alla volta
 
 $(document).ready(function () {
   
-
+  //Navbar dropdown menu
   $('.dropdown').click(function() {
 
     var $clickedDropdown = $(this).find('.dropdown-menu');
@@ -31,34 +31,33 @@ $(document).ready(function () {
   })
   
   //Hamburger Dropdown Menu Complete Vers
-  // $hambDropClose = $('.hamburger-dropdown .dropdown-close');
-  // $hambDropClose.click(function() {
-  //   console.log('click');
-  //   var $currentHambDrop = $(this).parents('.hamburger-dropdown');
-  //   var $currentHambDropMenu = $currentHambDrop.find('.hamburger-dropdown-menu');
+  $hambDropClose = $('.hamburger-dropdown .dropdown-close');
+  $hambDropClose.click(function() {
+    var $currentHambDrop = $(this).parents('.hamburger-dropdown');
+    var $currentHambDropMenu = $currentHambDrop.find('.hamburger-dropdown-menu');
 
-  //   if ($currentHambDropMenu.hasClass('active')) {
-  //     $currentHambDropMenu.removeClass('active');
-  //   } else if ($('.hamburger-dropdown-menu').hasClass('active')) {
-  //     $('.hamburger-dropdown-menu').removeClass('active');
-  //     $currentHambDropMenu.addClass('active'); //togliere per esclusione parziale
-  //   } else {
-  //     $currentHambDropMenu.addClass('active');
-  //   }
+    if ($currentHambDropMenu.hasClass('active')) {
+      $currentHambDropMenu.removeClass('active');
+    } else if ($('.hamburger-dropdown-menu').hasClass('active')) {
+      $('.hamburger-dropdown-menu').removeClass('active');
+      $currentHambDropMenu.addClass('active'); //togliere per esclusione parziale
+    } else {
+      $currentHambDropMenu.addClass('active');
+    }
     
-  // });
+  });
 
   //Hamburger Dropdown Menu Base senza Autoesclusione
   // $hambDropClose = $('.hamburger-dropdown .dropdown-close');
   // $hambDropClose.click(function () {
 
-  //   $currentHambDrop = $(this).parent('.hamburger-dropdown');
+  //   $currentHambDrop = $(this).parents('.hamburger-dropdown');
   //   $currentHambDrop.find('.hamburger-dropdown-menu').toggleClass('active');
 
   // });
 
   //Hamburger Dropdown Menu Base senza Autoesclusione Alt version meno specifica
-  //Non funziona più, funziona se dropdown-close viene assegnato a href 1 lvl sopra
+  //Non funziona più, funziona se dropdown-close viene assegnato -> a href 1 lvl sopra
   // $hambDropClose = $('.hamburger-dropdown .dropdown-close');
   // $hambDropClose.click(function() {
   //   $(this).next().toggleClass('active');
